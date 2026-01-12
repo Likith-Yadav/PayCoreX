bf5ced450842702de6ecaff5afc35b2177774566
Ubuntu <ubuntu@ip-172-31-30-169.ec2.internal>
2026-01-12 08:38:09 +0000

    payments

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('stats', views.stats, name='stats'),
-    path('payments', views.payments, name='payments'),
-    path('ledgers', views.ledgers, name='ledgers'),
-    path('payment-configs', views.payment_configs, name='payment_configs'),
-    path('payment-configs/<uuid:config_id>', views.payment_config_detail, name='payment_config_detail'),
+    path('stats', views.stats, name='dashboard_stats'),
+    path('payments', views.payments, name='dashboard_payments'),
+    path('ledgers', views.ledgers, name='dashboard_ledgers'),
+    path('payment-configs', views.payment_configs, name='dashboard_payment_configs'),
+    path('payment-configs/<uuid:config_id>', views.payment_config_detail, name='dashboard_payment_config_detail'),
 ]
 
